b58b6bafcf520bea77e94341c7721a60bb7ed8e4
Tyler Mello <tmello001@gmail.com>
2022-10-08 13:26:47 -0400

    finished create_cards method

--- deck.py
+++ deck.py
@@ -1,4 +1,13 @@
-import suit
+from suit import Suit
+from value import Value
+from card import Card
 class Deck:
     def __init__(self):
         self.Cards = []
+
+    def create_cards(self):
+        for suits in Suit:
+            for values in Value:
+                self.Cards.append(Card(suits, values))
+
+    
